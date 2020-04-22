04b8443f4fd04e239e34126caa1f00f932f3adaa
gelumalai <Gokul_E@intuit.com>
2020-04-22 19:45:23 +0530

    Add comments to DeepestLeavesSum.js

--- DeepestLeavesSum.js
+++ DeepestLeavesSum.js
@@ -1,25 +1,28 @@
-var deepestLeavesSum = function(root) {
-    
+var deepestLeavesSum = function (root) {
+
     let sum = 0;
     let maxDepth = 0;
-    
+
     const recurse = (node, depth) => {
-        if(!node) return;
-        if(!node.left && !node.right) {
-            if(depth+1 == maxDepth) {
+        if (!node) return;
+        if (!node.left && !node.right) {
+
+            // check this leaf has same depth we discovered before.
+            if (depth + 1 == maxDepth) {
                 sum += node.val;
             }
-            else  if(depth+1 > maxDepth) {
-                maxDepth = depth+1
+            // update new maxDepth when this leaf's depth is greater than maxDepth and start new sum.
+            else if (depth + 1 > maxDepth) {
+                maxDepth = depth + 1
                 sum = node.val;
             }
         }
-        recurse(node.left, depth+1);
-        recurse(node.right, depth+1);
+        recurse(node.left, depth + 1);
+        recurse(node.right, depth + 1);
     }
-    
+
     recurse(root, 0);
-    
+
     return sum;
-    
+
 };
